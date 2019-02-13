6f118390589b83e195e39e76d001bf4786e83716
Kostek707 <m.kostrzewa707@gmail.com>
2019-02-14 00:27:31 +0100

    Implement the sieve of Eratosthenes
    I used the sieve of Eratosthenes algorithm to find all prime numbers in a given range,
    which will allow me to check if the number is prime in constant time.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
 
 using namespace std;
-const long long RANGE = 1000000000;
-bool primeNumbers[RANGE];
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]){	
+	long long range;
+	cin >> range;
+	
+	range++;
+	
+	bool *primeNumbers;
+	primeNumbers = new bool [range];
+
+	for(long long i = 0; i<range; i++){
+		bool *pointer = primeNumbers+i;
+		*pointer=1;
+	}
+	bool *pointer = primeNumbers+2;
+	for(long long i = 2; i*i <= range; i++){
+		if(*pointer){
+			bool *pointer2 = primeNumbers+(i*i);
+			for(long long j = i*i; j<= range; j+=i){
+				*pointer2=false;
+				pointer2+=i;
+			}
+		}
+		pointer++;
+	}
+	primeNumbers[1]=0;
 	return 0;
 }
